254a824592325e192a1cc6fe05a500516df4e6de
rafee <zunaiedrafee@gmail.com>
2024-01-20 14:14:59 +0600

    changes

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def fetch_weather_data():
         return {"error": "Unable to fetch weather data"}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+    app.run( host='0.0.0.0',debug=True, port=5500)
